d2b9b15fda02f7163087b7ed4fbca1cd41a17a54
StellaIbeh <a.ibeh@alustudent.com>
2024-06-09 01:45:13 -0700

    normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,53 +1,97 @@
 
 #!/usr/bin/env python3
 
-"""Normal distribution"""
+'''Calculating normal distribution'''
+π = 3.1415926536
+e = 2.7182818285
 
 
 class Normal:
-    """ Normal distribution class
+    """
+    Represents a normal distribution.
+
+    Attributes:
+        mean (float): The mean of the distribution.
+        stddev (float): The standard deviation of the distribution.
     """
 
     def __init__(self, data=None, mean=0., stddev=1.):
-        """Initialization"""
-        self.E = 2.7182818285
-        self.PI = 3.1415926536
+        """
+        Initializes the Normal distribution with data or given mean and stddev.
 
+        Args:
+            data: List of the data to be used to estimate the distribution.
+            mean: The mean of the distribution.
+            stddev: The standard deviation of the distribution.
+
+        Raises:
+            ValueError: If stddev is not a positive value.
+            TypeError: If data is not a list.
+            ValueError: If data does not contain multiple values.
+        """
+        if stddev <= 0:
+            raise ValueError('stddev must be a positive value')
         if data is None:
-            if stddev <= 0:
-                raise ValueError('stddev must be a positive value')
-            else:
-                self.stddev = float(stddev)
             self.mean = float(mean)
+            self.stddev = float(stddev)
         else:
             if not isinstance(data, list):
                 raise TypeError('data must be a list')
             if len(data) < 2:
                 raise ValueError('data must contain multiple values')
-            self.mean = sum(data) / len(data)
-            self.stddev = (sum([(x - self.mean) ** 2 for x in data])
-                           / len(data)) ** 0.5
+            self.mean = float(sum(data) / len(data))
+            self.stddev = float(
+                (sum((x - self.mean) ** 2 for x in data) / len(data)) ** 0.5)
 
     def z_score(self, x):
-        """ Calculates the z-score of a given x-value"""
+        """
+        Calculates the z-score of a given x-value.
+
+        Args:
+            x: The x-value.
+
+        Returns:
+            float: The z-score of x.
+        """
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """ Calculates the x-value of a given z-score"""
+        """
+        Calculates the x-value of a given z-score.
+
+        Args:
+            z: The z-score.
+
+        Returns:
+            float: The x-value of z.
+        """
         return self.mean + z * self.stddev
 
     def pdf(self, x):
-        """ Calculates the value of the PDF for a given time period"""
-        return (self.E ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
-                / (self.stddev * ((2 * self.PI) ** 0.5)))
+        """
+        Calculates the value of the PDF for a given x-value.
+
+        Args:
+            x: The x-value.
+
+        Returns:
+            float: The PDF value for x.
+        """
+        return (1.0 / (self.stddev * (2 * π) ** 0.5)) * e ** (
+            -0.5 * ((x - self.mean) / self.stddev) ** 2)
 
     def cdf(self, x):
-        """ Calculates the value of the CDF for a given time period"""
-        return (0.5 * (1 + self.erf((x - self.mean) /
-                                    (self.stddev * 2 ** 0.5))))
+        """
+        Calculates the value of the CDF for a given x-value.
 
-    def erf(self, x):
-        """ Calculates the error function"""
-        return (2 / (self.PI ** 0.5)) * (x - (x ** 3) / 3 + (x ** 5) / 10
-                                         - (x ** 7) / 42 + (x ** 9) / 216)
+        Args:
+            x: The x-value.
+
+        Returns:
+            float: The CDF value for x.
+        """
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        t = z - (z ** 3) / 3 + (z ** 5) / 10 - (z ** 7) / 42 + (z ** 9) / 216
+        cdf = 0.5 * (1 + (2 / (π ** 0.5)) * t)
+        return cdf
 
