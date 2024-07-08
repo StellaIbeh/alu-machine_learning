08f2fdb84b5af5fe134d875e294a633999699b16
StellaIbeh <a.ibeh@alustudent.com>
2024-07-08 15:07:50 +0100

    done with all the work

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,33 +1,28 @@
 #!/usr/bin/env python3
-""" 
-   Performs convolution on grayscale images
-"""
+
+'''This script performs a valid convolution on grayscale images'''
+
 import numpy as np
 
 
+'''This function calculates the valid convolution of grayscale images'''
+
+
 def convolve_grayscale_valid(images, kernel):
-    """Function to convolve the image"""
-    if images.ndim == 4:
-        m, h, w, _ = images.shape
-        # Convert RGB to grayscale
-        images = (0.2989 * images[:, :, :, 0] +
-                  0.5870 * images[:, :, :, 1] +
-                  0.1140 * images[:, :, :, 2])
-    else:
-        m, h, w = images.shape
-
-    kh, kw = kernel.shape
-
-    # Calculate new dimensions after VALID convolution
+    '''args:
+        m is the number of images
+        h is the height in pixels of the images
+        w is the width in pixels of the images
+        kh is the height of the kernel
+        kw is the width of the kernel
+    '''
+    m, h, w = images.shape
+    kw, kh = kernel.shape
     new_h = h - kh + 1
     new_w = w - kw + 1
-
-    # Initialize the array for convolved images
-    output = np.zeros((m, new_h, new_w))
-
-    # Perform the convolution
+    convolved_images = np.zeros((m, new_h, new_w))
     for i in range(new_h):
         for j in range(new_w):
-            sub_matrix = images[:, i: i + kh, j: j + kw] * kernel
-            output[:, i, j] = np.sum(sub_matrix, axis=(1, 2))
-    return output
+            convolved_images[:, i, j] = np.sum(
+                images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+    return np.array(convolved_images)
--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 
 '''This script performs a same convolution on grayscale images'''
