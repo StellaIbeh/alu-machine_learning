988a27e5fdaf8b1fad938882b8be8f6a7125dc41
StellaIbeh <a.ibeh@alustudent.com>
2025-01-10 12:24:28 -0800

    3-neural_style.py

--- supervised_learning/neural_style_transfer/3-neural_style.py
+++ supervised_learning/neural_style_transfer/3-neural_style.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 """
 Defines class NST that performs tasks for neural style transfer
 """
 
+
 import numpy as np
 import tensorflow as tf
+
 
 class NST:
     """
