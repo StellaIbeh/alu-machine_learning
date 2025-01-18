ecdcf8c28a3fc498c6974ac84e449d23a2a4313d
StellaIbeh <a.ibeh@alustudent.com>
2025-01-18 12:57:35 +0200

    .\6-bi_backward.py

--- supervised_learning/RNNs/6-bi_backward.py
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -1,42 +1,33 @@
 #!/usr/bin/env python3
-
 """
-This module contains class BidirectionalCell which represents
-a Bidirectional cell of an RNN"""
+Script that defines a class BidirectionalCell
+"""
 import numpy as np
-
-
 class BidirectionalCell:
-    """class GRUCell"""
+    """
+    Class that represents a bidirectional cell of an RNN
+    """
     def __init__(self, i, h, o):
-        """class constructor
-        i - data dimensionality
-        h - hidden state dimensionality
-        o - outputs dimensionality
-        Whf, bhf - weights and bias for hidden states
-            in forward direction
-        Whb, bhb - weights and bias for hidden states
-            in backward direction
-        Wy, hy - weights and bias for output
         """
-        self.Whf = np.random.normal(size=(i + h, h))
-        self.Whb = np.random.normal(size=(i + h, h))
-        self.Wy = np.random.normal(size=(2 * h, o))
-
+        Class constructor
+        """
         self.bhf = np.zeros((1, h))
         self.bhb = np.zeros((1, h))
         self.by = np.zeros((1, o))
-
+        self.Whf = np.random.normal(size=(h + i, h))
+        self.Whb = np.random.normal(size=(h + i, h))
+        self.Wy = np.random.normal(size=((2 * h), o))
     def forward(self, h_prev, x_t):
-        """calculates hidden state in forward direction
-        for one time step
-        x_t - cell data input shape(m, i)
-            m - batch size
-        h_prev - previous hidden state shape(m, h)
-        returns h_next
-        h_next - next hidden state
         """
-        # concatenate
-        h_x_concat = np.concatenate((h_prev, x_t), axis=1)
-        h_next = np.tanh(np.dot(h_x_concat, self.Whf) + self.bhf)
+        Function that performs forward propagation
+        """
+        h_x = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(h_x, self.Whf) + self.bhf)
         return h_next
+    def backward(self, h_next, x_t):
+        """
+        Function that performs backward propagation
+        """
+        h_x = np.concatenate((h_next, x_t), axis=1)
+        h_prev = np.tanh(np.matmul(h_x, self.Whb) + self.bhb)
+        return h_prev
