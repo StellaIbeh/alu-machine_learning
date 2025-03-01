5ae03c89bca7bad83a88d9d40b8572f9fe928889
StellaIbeh <a.ibeh@alustudent.com>
2025-03-01 05:56:48 +0200

    Attention

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -1,31 +1,76 @@
 #!/usr/bin/env python3
-""" doc """
+"""
+A class RNNEncoder that inherits from
+tensorflow.keras.layers.Layer to encode for machine translation:
+"""
+
 
 import tensorflow as tf
 
 
 class RNNEncoder(tf.keras.layers.Layer):
-    """ doc """
+    """
+    class RNNEncoder:
+    """
 
     def __init__(self, vocab, embedding, units, batch):
-        """ doc """
+        """
+        Class constructor
+        Args:
+            vocab: the size of the input vocabulary
+            embedding: the dimensionality of the embedding vector
+            units: the number of hidden units in the GRU cell
+            batch: the batch size
+        """
+        if type(vocab) is not int:
+            raise TypeError(
+                "vocab must be int representing the size of input vocabulary"
+            )
+        if type(embedding) is not int:
+            raise TypeError(
+                "embedding must be int representing dimensionality of vector"
+            )
+        if type(units) is not int:
+            raise TypeError(
+                "units must be int representing the number of hidden units"
+            )
+        if type(batch) is not int:
+            raise TypeError("batch must be int representing the batch size")
         super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
-        self.embedding = tf.keras.layers.Embedding(input_dim=vocab,
-                                                   output_dim=embedding)
-        self.gru = tf.keras.layers.GRU(units=units,
-                                       recurrent_initializer='glorot_uniform',
-                                       return_sequences=True,
-                                       return_state=True)
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(
+            self.units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer="glorot_uniform",
+        )
 
     def initialize_hidden_state(self):
-        """ doc """
-        init = tf.keras.initializers.Zeros()
-        return (init(shape=(self.batch, self.units)))
+        """
+        Initializes the hidden states for the
+        RNN cell to a tensor of zeros
+
+        Returns:
+            -  tensor of shape (batch, units)containing
+            the initialized hidden states
+        """
+        hidden_states = tf.zeros(shape=(self.batch, self.units))
+        return hidden_states
 
     def call(self, x, initial):
-        """ doc """
-        embedding = self.embedding(x)
-        outputs, hidden = self.gru(embedding, initial_state=initial)
+        """
+        call one
+
+        parameters:
+            x: data
+            initial: initial hidden state
+
+        return:
+            outputs: the outputs of the RNN
+            hidden: the hidden states of the RNN
+        """
+        x = self.embedding(x)
+        outputs, hidden = self.gru(x, initial_state=initial)
         return outputs, hidden
